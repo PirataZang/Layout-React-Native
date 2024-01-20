16649b5043240297a5272140ff37f0a22ed72e1a
Igor Fronza <igorfronza20@gmail.com>
2024-01-20 00:31:54 -0300

    New

---
+++ mobile/src/pages/Folder.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function folder() {
+  return (
+    <View>
+      <Text>folder</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
